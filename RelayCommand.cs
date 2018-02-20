9b62aa813368fed62cc7b4a3592c9f7a6bc21c79
Andy Myers <jamyers@andy.local>
2018-02-19 21:26:22 -0500

    Initial commit

---
+++ RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPFRelayCommand
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action<object> execute;
+        readonly Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new NullReferenceException();
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+
+        }
+
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute.Invoke(parameter);
+
+        }
+    }
+}
---
+++ ViewModel/MessageViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WPFRelayCommand.ViewModel
+{
+    public class MessageViewModel
+    {
+        public ObservableCollection<string> MyMessages { get; private set; }
+
+        public RelayCommand MessageBoxCommand { get; private set; }
+        public RelayCommand ConsoleLogCommand { get; private set; }
+
+        public MessageViewModel()
+        {
+            MyMessages = new ObservableCollection<string>()
+            {
+                "Hello Dorks!",
+                "Howdy",
+                "How are you today?",
+                "I'm a message box",
+                "What's the deal with airline pretzels?",
+                "Have a nice day.",
+                "I'm a console"
+            };
+
+            MessageBoxCommand = new RelayCommand(DisplayInMessageBox, MessageBoxCanUse);
+            ConsoleLogCommand = new RelayCommand(DisplayConsole, ConsoleCanUse);
+        }
+
+        public void DisplayInMessageBox(object message)
+        {
+            MessageBox.Show((string)message);
+        }
+
+        public void DisplayConsole(object message)
+        {
+            Console.WriteLine((string)message);
+        }
+
+        public bool MessageBoxCanUse(object message)
+        {
+            if ((string)message == "I'm a console")
+                return false;
+
+            return true;
+        }
+
+        public bool ConsoleCanUse(object message)
+        {
+            if ((string)message == "I'm a message box")
+                return false;
+
+            return true;
+        }
+    }
+}
